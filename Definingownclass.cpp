d37fedf30cebb7b0d08d8624f12058123ca0a056
Irshathvasur <irshathvasur@gmail.com>
2022-02-08 14:06:17 +0530

    Defining own Class with struct

---
+++ Definingownclass.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+struct students
+{
+    std::string name; 
+    int id, age;
+    float percentage;
+};
+int main()
+{
+students stu;
+int age;
+int id;
+float mark;
+system("cls");
+std::cout<<"Enter the second student name-";
+std::string stuname;
+std::cin>>stuname;
+std::cout<<"Enter the age of "<<stuname;
+std::cin>>age;
+std::cout<<"Enter the ID no of "<<stuname;
+std::cin>>id;
+std::cout<<"Enter the Mark of "<<stuname;
+std::cin>>mark;
+stu={stuname, id, age, mark};
+std::cout<<"Students details are"<<std::endl;
+std::cout<<"Name-"<<stu.name<<std::endl;
+std::cout<<"Age-"<<stu.age<<std::endl;
+std::cout<<"ID-"<<stu.id<<std::endl;
+std::cout<<"Mark-"<<stu.percentage<<std::endl;
+getch();
+return 0;
+}
